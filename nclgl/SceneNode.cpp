13aba8f4889d7127f71adcc76486c247977a5087
Jerry <j.li216@newcastle.ac.uk>
2024-11-08 14:09:02 +0000

    SceneNode .cpp .h

---
+++ nclgl/SceneNode.cpp
@@ -0,0 +1,37 @@
+﻿#include "SceneNode.h"
+
+SceneNode::SceneNode(Mesh* mesh, Vector4 colour) {
+    this->mesh = mesh;
+    this->colour = colour;
+    parent = NULL;
+    modelScale = Vector3(1, 1, 1);
+}
+
+//不会删除Mesh变量，只是删除节点，因此可以多个SceneNode指向同的网格实例，而不必每个节点都拥有自己的 Mesh
+SceneNode::~SceneNode(void) {
+    for (unsigned int i = 0; i < children.size(); ++i) {
+        delete children[i];
+    }
+}
+
+//添加物体到子节点中，并将其父节点设为当前节点
+void SceneNode::AddChild(SceneNode* s) {
+    children.push_back(s);
+    s->parent = this;
+}
+
+void SceneNode::Draw(const OGLRenderer& r) {
+    if (mesh) { mesh->Draw(); }
+}
+
+void SceneNode::Update(float dt) {
+    if (parent) {
+        worldTransform = parent->worldTransform * transform;
+    } else {
+        worldTransform = transform;
+    }
+
+    for (auto i = children.begin(); i != children.end(); ++i) {
+        (*i)->Update(dt);
+    }
+}
---
+++ nclgl/SceneNode.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include "Matrix4.h"
+#include "Vector3.h"
+#include "Vector4.h"
+#include "Mesh.h"
+#include <vector>
+
+class SceneNode
+{
+public:
+    SceneNode(Mesh* mesh=NULL, Vector4 colour=Vector4(1,1,1,1));
+
+    //析构销毁所有子节点
+    ~SceneNode();
+
+    //位置
+    const Matrix4& GetTransform() const { return transform; }
+    Matrix4 GetWorldTransform() const { return worldTransform; }
+
+    //颜色
+    Vector4 GetColour() const { return colour; }
+    void SetColour(Vector4 c) { colour = c; }
+
+    //缩放
+    Vector3 GetModelScale() const { return modelScale; }
+    void SetModelScale(Vector3 s) { modelScale = s; }
+
+    //模型
+    Mesh* GetMesh() const { return mesh; }
+    void SetMesh(Mesh* m) { mesh = m; }
+
+    //添加子物体 绘制
+    void AddChild(SceneNode* s);
+    virtual void Update(float dt);
+    virtual void Draw(const OGLRenderer& r);
+
+    //遍历子物体的迭代器
+    std::vector<SceneNode*>::const_iterator GetChildIteratorStart() { return children.begin(); }
+    std::vector<SceneNode*>::const_iterator GetChildIteratorEnd() { return children.end(); }
+
+protected:
+    SceneNode* parent;
+    Mesh* mesh;
+    Matrix4 worldTransform;
+    Matrix4 transform;
+    Vector3 modelScale;
+    Vector4 colour;
+    std::vector<SceneNode*> children;
+
+};
